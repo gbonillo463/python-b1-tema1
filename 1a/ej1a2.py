c5091da7fc7aa1709ad311b8c927d646bdebab2d
gbonillo463 <gbonillo@uoc.edu>
2025-12-29 12:24:34 +0100

    Update ej1a2.py

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -13,38 +13,36 @@ Parámetros:
 - list_numbers: Lista de números enteros positivos.
 
 Ejemplo:
-    Entrada:
-    sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
+	Entrada:
+	sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
 
-    Salida:
-    30
-
-Enunciat:
-Crea una funció 'sum_odd_numbers(list_numbers)' que rebi com
-paràmetre una llista de nombres positius enters anomenada 'list_numbers'
-i torneu la suma dels números imparells trobats a la llista.
-Considereu que 'list_numbers' ha de contenir valors numèrics enters majors
-o iguals a '0', en cas contrari cal mostrar un error tipus 'ValueError'.
-
-L'error el pots mostrar amb la següent instrucció:
-raise ValueError("MISSATGE D'ERROR")
-
-Paràmetres:
-- list_numbers: Llista de nombres enters positius.
-
-Exemple:
-     Entrada:
-     sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
-
-     Sortida:
-     30
+	Salida:
+	30
 
 '''
 
+def is_odd(number) -> bool:
+	return number % 2 != 0
+
+
 def sum_odd_numbers(list_numbers):
-    # Write here your code
-    pass
+	
+	total_odd_sum = 0
+
+	for number in list_numbers:
+
+		if not all(isinstance(n, int) for n in list_numbers):
+			raise ValueError("Todos los valores deben ser enteros")
+	
+		if not all(n >= 0 for n in list_numbers):
+			raise ValueError("Todos los valores deben ser >= 0")
+		
+		elif is_odd(number):
+			total_odd_sum += number
+	
+	return total_odd_sum
+
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
+print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
